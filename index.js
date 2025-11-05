52dafee86811484acdfe9d22eff33289b7a7a610
Nikhil8306 <nikhileng83@gmail.com>
2025-11-05 16:09:31 +0530

    Simple express server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello There")
+})
+
+
+app.listen(5000, () => {
+    console.log("Server running !");   
+})

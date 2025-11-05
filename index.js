921f53878f3365dbfa4fefed772fbd765ae71b22
Nikhil8306 <nikhileng83@gmail.com>
2025-11-05 18:48:51 +0530

    Secrete check

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import express from "express";
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 const app = express();
 
 
 app.get("/", (req, res) => {
     res.send("Hello There")
+})
+
+app.get("/", async (_, res) => {
+    const secret_name = "testenvs";
+
+    const client = new SecretsManagerClient({
+        region: "eu-north-1",
+    });
+
+    let response;
+
+    try {
+    response = await client.send(
+        new GetSecretValueCommand({
+            SecretId: secret_name,
+        })
+    );
+    } catch (error) {
+        throw error;
+    }
+
+    const secret = response.SecretString;
+
+    return res.send("Your secret is : ". secret);
 })
 
 

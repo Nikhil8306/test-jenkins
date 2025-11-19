0e6864580b185eb498d83b26b610fa80d8bd6a63
Nikhil8306 <nikhileng83@gmail.com>
2025-11-19 17:12:01 +0530

    Test Commit

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 import express from "express";
 import {
-    SecretsManagerClient,
-    GetSecretValueCommand,
+  SecretsManagerClient,
+  GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
 const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Hello There")
+  res.send("Hello There")
 })
 
 app.get("/secret", async (_, res) => {
-    const secret_name = "testenvs";
+  const secret_name = "testenvs";
 
-    const client = new SecretsManagerClient({
-        region: "eu-north-1",
-    });
+  const client = new SecretsManagerClient({
+    region: "eu-north-1",
+  });
 
-    let response;
+  let response;
 
-    try {
+  try {
     response = await client.send(
-        new GetSecretValueCommand({
-            SecretId: secret_name,
-        })
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+      })
     );
-    } catch (error) {
-        throw error;
-    }
+  } catch (error) {
+    throw error;
+  }
 
-    const secret = response.SecretString;
+  const secret = response.SecretString;
 
-    return res.send("Your secret is : ". secret);
+  return res.send("Your secret is : ".secret);
 })
 
 
 app.listen(9000, () => {
-    console.log("Server running !");   
+  console.log("Server running !");
 })
 
